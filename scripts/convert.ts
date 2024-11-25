0380a985942b0a08b31056b48c8825bb94666401
Josh Yang <kauhentus@gmail.com>
2024-11-24 22:15:28 -0500

    updated extract ring script

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -10,7 +10,7 @@ const vid_to_splat = async (path: string, id: number) => {
     const file_name = path.split("/").at(-1)
     const temp_copy_path = `./temp-frames/${file_name}`;
     fs.copyFileSync(path, temp_copy_path);
-    execSync(`ffmpeg -i ${temp_copy_path} -r 4 ./temp-frames/output_%04d.jpg`);
+    execSync(`ffmpeg -i ${temp_copy_path} -r 4  -color_trc smpte2084 -color_primaries bt2020 -pix_fmt rgb24 ./temp-frames/output_%04d.jpg`);
     fs.rmSync(temp_copy_path);
 
     const temp_scene_folder = `${root_path_of_mvs}/examples/scene_vid_${id}`;
@@ -21,7 +21,7 @@ const vid_to_splat = async (path: string, id: number) => {
     const frame_paths = frame_names.map(n => `./temp-frames/${n}`);
     frame_paths.forEach((p, i) => fs.copyFileSync(p, `${temp_scene_folder}/images/${frame_names[i]}`));
     execSync(`python ${root_path_of_mvs}/lib/colmap/imgs2poses.py -s ${root_path_of_mvs}/examples/scene_vid_${id}`);
-    fs.rmSync('./temp-frames/', { recursive: true, force: true });
+    // fs.rmSync('./temp-frames/', { recursive: true, force: true });
 
     // TRAIN
     // execSync(`python ${root_path_of_mvs}/lib/train.py --eval --iterations 1000 -s ${root_path_of_mvs}/examples/scene_temp`);
